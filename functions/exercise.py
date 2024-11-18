1cbcd1e09d967f06b2b8c79c4e91c27d2ec6d63b
Alok Kumar <136910791+alokkumar777@users.noreply.github.com>
2024-11-18 13:46:49 +0530

    update

--- functions/exercise.py
+++ functions/exercise.py
@@ -1,12 +1,26 @@
 # Exercise level-3
 # Write a function called is_prime, which checks if a number is prime.
 
-def isPrime(num: int) -> bool:
+def isPrime(num: int) -> str:
     return [
         "Prime" if num % i != 0 else "Not Prime" 
         for i in range(2, int(num ** 0.5) + 1)
         ]
         
-print(isPrime(8))
+# print(isPrime(8))
 
+def checkUnique(lst: list) -> bool:
+    return len(lst) == len(set(lst))
+
+lst = [1, 2, 3, 4, 4]
+# print(checkUnique(lst))
+
+def allSameType(lst) -> bool:
+    if not lst:
+        return  True
+    first_type = type(lst[0])
+    return all(isinstance(i, first_type) for i in lst)
+
+print(allSameType([1, 2, 5]))
+    
 
